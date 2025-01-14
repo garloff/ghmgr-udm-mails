572f0fb3b94f9b4539641c79fb6aa37c91f215dc
Kurt Garloff <kurt@garloff.de>
2025-01-14 17:35:50 +0100

    Add License, initial version of script, README.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # ghmgr-udm-mails
-Find mail addresses for github org members (gh-manager managed) in UCS user directory
+
+Find mail addresses for github org members (gh-manager managed) in UCS user directory.
+
+## Why this tool?
+
+For the [SCS](https://scs.community/) [project board](https://docs.scs.community/standards/scs-0005-v1-project-governance)
+election, all members of the SCS github organization have active voting rights.
+The members of the organization are defined in the
+[data.yaml](https://github.com/SovereignCloudStack/github-manager/blob/main/orgs/SovereignCloudStack/data.yaml)
+file in github-manager.
+
+In order to use the voting right, users need to receive a link with a token via email.
+The election management thus needs a list of email addresses of all voters.
+
+The data in github-manager only contains the github handles and -- in most cases --
+the full names of the voters, but no email address.
+
+Some github users have configured a publicly visible email address. This one can be used
+and can be queried via the github API, using a
+[personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens).
+
+The other database that contains email addresses is the
+[UCS](https://www.univention.com/products/ucs/)/[nextcloud](https://nextcloud.com/)
+server that the SCS community has been using since Dec 2019. Mailing lists are also operated there.
+
+Using the full names, github users can be matched with UCS/nextcloud users and the email
+address(es) be extracted. (Matching is a bit tricky in presence of titles, umlauts etc.
+so some normalization is done.) This is what this script does, creating a .csv file with
+github handles, full names, UCS account names and email addresses from github
+manager's data.yaml file, the UCS user database dump (`udm user/users list`) and
+(optionally) the public mail addresses via the github API.
+
+## Usage
+
+```
+scs-mails.py [-o out.csv] [-p github.pat] data.yaml users.udm
+```
---
+++ scs-mails.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+#
+# scs-mails.py
+#
+# This program looks through all github accounts as listed in a
+# github-manager org data.yaml file.
+# It then queries github for a public mail address.
+# It additionally looks through a UCS users file for mail addresses,
+# using some fuzzy matching logic for names.
+#
+# The output will be a CSV file with github acct name, name, mails
+#
+# (c) Kurt Garloff <scs@garloff.de>, 1/2025
+# SPDX-License-Identifier: CC-BY-SA-4.0
+
+# import os
+import sys
+import getopt
+import re
+import github
+import yaml
+
+SEP = ","
+
+
+def usage():
+    "Help"
+    print("Usage: scs-mails.py [-o out.csv] [-p github.pat] data.yaml users.udm")
+    return 1
+
+
+class MailUser:
+    "An github ORG member"
+    def __init__(self, ghnm, name):
+        "C'tor"
+        self.ghnm = ghnm
+        self.name = name
+        self.ucsnm = ""
+        self.mls = []
+
+    def __str__(self):
+        "String representation (CSV)"
+        return f"{self.ghnm}{SEP}{self.name}{SEP}{self.ucsnm}{SEP}{SEP.join(self.mls)}"
+
+
+def parseOrgData(fnm):
+    "Read github-manager org data.yaml and return user list"
+    users = []
+    with open(fnm, 'r', encoding="utf-8") as file:
+        y = yaml.safe_load(file)
+    for member in y["members"]:
+        users.append(MailUser(member["login"], member["name"]))
+    return users
+
+
+def githubMail(user, gh):
+    "Try to get github public mail for user and add"
+    ghu = gh.get_user(user.ghnm)
+    if ghu.email:
+        if ghu.email not in user.mls:
+            user.mls.append(ghu.email)
+
+
+class UdmUser:
+    "Abstraction of UCS user (as output by udm udm users/user list)"
+    def __init__(self, uid, name=""):
+        "C'tor"
+        self.uid = uid
+        self.name = name
+        self.gecos = name
+        self.mls = []
+
+    def __repr__(self):
+        "string representation"
+        return f"{self.uid}{SEP}{self.name}{SEP}{self.gecos}{SEP}{SEP.join(self.mls)}"
+
+
+def readUDM(fnm):
+    "parse UDM user dump (LDAP like)"
+    uidre = re.compile(r'uid=([^,]*),')
+    udm = []
+    usr = None
+    with open(fnm, "r", encoding="utf-8") as file:
+        for ln in file:
+            ln = ln.strip('\r\n')
+            # A new record
+            if ln[:4] == "DN: ":
+                m = uidre.match(ln[4:])
+                if m:
+                    usr = UdmUser(m.group(1))
+                    udm.append(usr)
+                else:
+                    usr = None
+                    print(f"Warning: DN line without uid= match: {ln}", file=sys.stderr)
+            # If we're not inside a record, ignore
+            if not usr:
+                continue
+            # Parse Name and eMails
+            if ln[2:14] == "displayName:":
+                usr.name = ln[15:]
+            elif ln[2:21] == "mailForwardAddress:":
+                if ln[22:] in usr.mls:
+                    usr.mls.remove(ln[22:])
+                usr.mls.insert(0, ln[22:])
+            elif ln[2:9] == "e-mail:":
+                if ln[10:] not in usr.mls:
+                    usr.mls.append(ln[10:])
+            elif ln[2:8] == "gecos:":
+                usr.gecos = ln[9:]
+    return udm
+
+
+def normalizeName(nm):
+    "Normalize Name to avoid non-ASCII chars and to strip Dr."
+    if nm[:3] == "Dr.":
+        nm = nm[3:]
+    nm = nm.strip(" ")
+    nm = nm.replace("ä", "ae")
+    nm = nm.replace("ö", "oe")
+    nm = nm.replace("ü", "ue")
+    nm = nm.replace("Ä", "Ae")
+    nm = nm.replace("Ö", "Oe")
+    nm = nm.replace("Ü", "Ue")
+    nm = nm.replace("ß", "ss")
+    nm = nm.replace("é", "e")
+    nm = nm.replace("è", "e")
+    nm = nm.replace("ë", "e")
+    nm = nm.replace("å", "a")
+    nm = nm.replace("æ", "ae")
+    nm = nm.replace("œ", "oe")
+    nm = nm.replace("ø", "o")
+    nm = nm.replace("ĳ", "ij")
+    nm = nm.replace("ÿ", "y")
+    nm = nm.replace("ý", "y")
+    nm = nm.replace("š", "s")
+    nm = nm.replace("č", "c")
+    nm = nm.replace("ç", "c")
+    nm = nm.replace("ñ", "n")
+    nm = nm.replace("ł", "l")
+    nm = nm.replace("-", " ")
+    return nm
+
+
+def nameMatch(nm1, nm2):
+    "Are these names the same, at least after normalization?"
+    if nm1 == nm2:
+        return True
+    if normalizeName(nm1) == normalizeName(nm2):
+        return True
+    # We could do more here? Fuzzy Logic?
+    return False
+
+
+def udmMail(user, udm):
+    "Search udm database for mail address and add uid and mails if there's a match"
+    for udmuser in udm:
+        if nameMatch(user.name, udmuser.name) or nameMatch(user.name, udmuser.gecos):
+            user.ucsnm = udmuser.uid
+            for ml in udmuser.mls:
+                user.mls.append(ml)
+
+
+def main(argv):
+    "main entry point"
+    # Defaults
+    out = sys.stdout
+    pat = None
+    # Option handling
+    try:
+        (optlist, args) = getopt.gnu_getopt(argv[1:], "ho:p:",
+                                            ("help", "outfile", "pat"))
+    except getopt.GetoptError as exc:
+        print("Error:", exc, file=sys.stderr)
+        sys.exit(usage())
+    for opt in optlist:
+        if opt[0] == "-h" or opt[0] == "--help":
+            usage()
+            sys.exit(0)
+        elif opt[0] == "-o" or opt[0] == "--outfile":
+            out = open(opt[1], "w", encoding="utf-8")
+        elif opt[0] == "-p" or opt[0] == "--pat":
+            pat = opt[1]
+
+    if not args:
+        sys.exit(usage())
+    # Create user list
+    users = parseOrgData(args[0])
+    # Add mails from UDM dump
+    if len(args) > 1:
+        udm = readUDM(args[1])
+        for user in users:
+            udmMail(user, udm)
+    # Add mails from github
+    if pat:
+        gha = github.Auth.Token(open(pat, "r").read().strip('\r\n'))
+        with github.Github(auth=gha) as gh:
+            for user in users:
+                githubMail(user, gh)
+
+    for user in users:
+        print(user, file=out)
+
+
+# Call main
+if __name__ == "__main__":
+    main(sys.argv)
